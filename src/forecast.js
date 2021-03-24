92474ebafce2c9c4049e76f64e32232d343cbbf3
Jessi <jessivelazq1@gmail.com>
2021-03-23 19:59:07 -0700

    rendered forecast data on card

---
+++ src/forecast.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+
+class Forecast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      weatherForecast:[],
+      city: ''
+    }
+  }
+
+  componentDidMount = async() => {
+    const SERVER = 'http://localhost:3001';
+    const forecast = await axios.get(`${SERVER}/weather`);
+    const forecastArray = forecast.data.forecast;
+    const cityData = forecast.data.city;
+    this.setState({ city: cityData })
+    this.setState({ weatherForecast: forecastArray });
+  }
+
+  forecastRender() {
+    const data = this.state.weatherForecast;
+    return <div><h2>Weather Forecast:</h2><ul>{data.map(item => <li>{item.date}: {item.description}</li>)}</ul></div>
+  }
+
+  render() {
+    return(
+      <>
+        {this.forecastRender()}
+      </>
+
+    )
+  }
+}
+
+export default Forecast;
